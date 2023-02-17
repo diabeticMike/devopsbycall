0d45cdb6146109f4fd49e78701e16392728f6acf
Ruslan Hovorukha <ruslan.hovoruha@gmail.com>
2023-02-17 16:35:30 +0200

    Final

---
+++ 001_user_database/go.mod
@@ -0,0 +1,3 @@
+module 001_user_database
+
+go 1.20
--- 001_user_database/main.go
+++ 001_user_database/main.go
@@ -1,29 +1,48 @@
 package main
 
+import (
+	"fmt"
+)
+
 type user struct {
 	name  string
 	age   uint8
 	email string
 }
 
-users := []user{
-	{
-		name:  "Mike",
-		age:   32,
-		email: "mike@gmail.com",
-	},
-	{
-		name:  "John",
-		age:   54,
-		email: "john@gmail.com",
-	},
-	{
-		name:  "Abobus",
-		age:   2,
-		email: "abobus@gmail.com",
-	},
-
 func main() {
-
+	users := []user{
+		{
+			name:  "Mike",
+			age:   32,
+			email: "mike@gmail.com",
+		},
+		{
+			name:  "John",
+			age:   54,
+			email: "john@gmail.com",
+		},
+		{
+			name:  "Abobus",
+			age:   2,
+			email: "abobus@gmail.com",
+		},
 	}
+
+	//fmt.Println(users[len(users)-1])
+	//fmt.Println(len(users))
+
+	var clientInfo string = "John"
+	fmt.Println("Введенное имя для поиска: " + clientInfo)
+	//fmt.Println(users[0].name)
+
+	for index := 0; index < len(users); index++ {
+		if users[index].name == clientInfo {
+			fmt.Println("****************Информация об клиенте****************")
+			fmt.Println("Имя клиента: " + users[index].name)
+			fmt.Println("Email клиента: " + users[index].email)
+			fmt.Println("Возвраст клиента: " + fmt.Sprint(users[index].age))
+		}
+	}
+
 }
